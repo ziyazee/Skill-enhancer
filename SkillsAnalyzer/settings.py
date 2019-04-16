44bffd6a555741187541bdbf2679371f987e53e0
ziyazee <4nm15is029@gmail.com>
2019-04-16 09:40:35 +0530

    initial commit

--- SkillsAnalyzer/settings.py
+++ SkillsAnalyzer/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '-ufx79v8e%7mu=sz3p5)=#lon!^ik*&zhm1%&urg&--a*n@y(-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['SkillEnhancer.pythonanywhere.com','127.0.0.1','localhost']
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
